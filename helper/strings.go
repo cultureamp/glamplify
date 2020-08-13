60d3c3a199a203c5a865ab8804bda9ce997a52c3
Michael Ridgway <michael.ridgway@gmail.com>
2020-08-14 08:25:36 +1000

    Added string Redact func

--- helper/strings.go
+++ helper/strings.go
@@ -5,6 +5,7 @@ import (
 	"unicode"
 )
 
+// TOSnakeCase returns a new string in the format word_word
 func ToSnakeCase(s string) string {
 
 	var sb strings.Builder
@@ -29,3 +30,34 @@ func ToSnakeCase(s string) string {
 
 	return sb.String()
 }
+
+// Redact returns a string with some of the string replaced with *
+func Redact(s string) string {
+	const stars = 6
+	const literals = 4
+
+	l := len(s)
+	var b strings.Builder
+	b.Grow(l+stars)
+
+	// no matter how long the string, show at least 6 "*"
+	for i := 0; i < stars; i++ {
+		b.WriteString("*")
+	}
+
+	if l <= stars {
+		// For small strings, always return "******" don't suffix with any literal chars
+		return b.String()
+	}
+
+	// For larger strings, redact the first n-chars, and keep the last 4 as is
+	r := l - stars // how many of the last chars to print
+	if r > literals {
+		r = literals // we never print more than the last 4 chars
+	}
+	r = l - r // index of last chars
+
+	b.WriteString(s[r:])
+	return b.String()
+}
+
--- helper/strings_test.go
+++ helper/strings_test.go
@@ -29,7 +29,50 @@ func Test_ToSnakeCase(t *testing.T) {
 	assert.Assert(t,  sc == "and_with_capitals","was: '%s'", sc)
 }
 
-func BenchmarkLogging(b *testing.B) {
+func Test_Redact(t *testing.T) {
+	s := ""
+	r := Redact(s)
+	assert.Assert(t, r == "******", r)
+
+	s = "1234"
+	r = Redact(s)
+	assert.Assert(t, r == "******", r)
+
+	s = "123456"
+	r = Redact(s)
+	assert.Assert(t, r == "******", r)
+
+	s = "1234567"
+	r = Redact(s)
+	assert.Assert(t, r == "******7", r)
+
+	s = "12345678"
+	r = Redact(s)
+	assert.Assert(t, r == "******78", r)
+
+	s = "123456789"
+	r = Redact(s)
+	assert.Assert(t, r == "******789", r)
+
+	s = "1234567890"
+	r = Redact(s)
+	assert.Assert(t, r == "******7890", r)
+
+	s = "12345678901"
+	r = Redact(s)
+	assert.Assert(t, r == "******8901", r)
+
+	s = "123456789012"
+	r = Redact(s)
+	assert.Assert(t, r == "******9012", r)
+
+	s = "1234567890123"
+	r = Redact(s)
+	assert.Assert(t, r == "******0123", r)
+
+}
+
+func Benchmark_ToSnakeCase(b *testing.B) {
 
 	sa := []string {"hello", "requestID", "request_id", "something happened"}
 
