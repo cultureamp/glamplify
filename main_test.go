e651f5590ba25fcce0779fefbf56a41d6f0737da
techmanmike <michael.ridgway@gmail.com>
2020-12-10 12:05:21 +1100

    fixed coverage step

---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// call flag.Parse() here if TestMain uses flags
+	runExitCode := m.Run()
+
+	// runExitCode 0 means we've passed,
+	// and CoverMode will be non empty if run with -cover
+	if runExitCode == 0 && testing.CoverMode() != "" {
+		coverageResult := testing.Coverage()
+
+		// If we are less than 80% then fail the build
+		if coverageResult < 0.5 {
+			fmt.Printf("Tests passed but coverage failed: MUST BE >= 50%%, was %.2f\n", coverageResult*100)
+			runExitCode = -1
+		}
+	}
+	os.Exit(runExitCode)
+}
+
