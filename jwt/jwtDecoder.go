3bb2594e9a07b8a1c359fd65405a97e64fad1f67
Don Buchanan <42761041+Don-CA@users.noreply.github.com>
2023-08-10 16:16:29 +1000

    Revert "feat: add the ability to have multiple public keys"
    This reverts commit ad53d17fa0229f2f7066743ff28786a8a31b6e49.

--- jwt/jwtDecoder.go
+++ jwt/jwtDecoder.go
@@ -13,8 +13,7 @@ import (
 
 // Decoder represents how to decode a JWT
 type Decoder struct {
-	defaultVerifyKey *rsa.PublicKey            // Web Gateway does not provide a kid header
-	additionalKeys   map[string]*rsa.PublicKey // Optional to accept jwts signed by other services
+	verifyKey *rsa.PublicKey
 }
 
 // NewDecoder creates a new Decoder
@@ -31,23 +30,9 @@ func NewDecoderFromPath(pubKeyPath string) (Decoder, error) {
 
 // NewDecoderFromBytes creates a new Decoder given the public key as a []byte
 func NewDecoderFromBytes(verifyBytes []byte) (Decoder, error) {
-	return NewMultiKeyDecoderFromBytes(verifyBytes, make(map[string][]byte))
-}
-
-// NewDecoderFromBytes creates a new Decoder given the public key as a []byte
-func NewMultiKeyDecoderFromBytes(verifyBytes []byte, additionalVerifyBytes map[string][]byte) (Decoder, error) {
 	verifyKey, err := jwtgo.ParseRSAPublicKeyFromPEM(verifyBytes)
-	additionalKeys := make(map[string]*rsa.PublicKey)
-	for key, bytes := range additionalVerifyBytes {
-		additionalKey, err := jwtgo.ParseRSAPublicKeyFromPEM(bytes)
-		if err != nil {
-			return Decoder{}, err
-		}
-		additionalKeys[key] = additionalKey
-	}
 	return Decoder{
-		defaultVerifyKey: verifyKey,
-		additionalKeys:   additionalKeys,
+		verifyKey: verifyKey,
 	}, err
 }
 
@@ -59,23 +44,22 @@ func (jwt Decoder) Decode(tokenString string) (Payload, error) {
 	data := Payload{}
 
 	token, err := jwtgo.Parse(tokenString, func(token *jwtgo.Token) (interface{}, error) {
-		return fetchPublicKey(token, jwt), nil
+		return jwt.verifyKey, nil
 	})
-
 	if err != nil {
 		return data, err
 	}
 
 	if claims, ok := token.Claims.(jwtgo.MapClaims); ok && token.Valid {
-		data.Customer, err = jwt.extractData(claims, "accountId")
+		data.Customer, err = jwt.extractKey(claims, "accountId")
 		if err != nil {
 			return data, err
 		}
-		data.RealUser, err = jwt.extractData(claims, "realUserId")
+		data.RealUser, err = jwt.extractKey(claims, "realUserId")
 		if err != nil {
 			return data, err
 		}
-		data.EffectiveUser, err = jwt.extractData(claims, "effectiveUserId")
+		data.EffectiveUser, err = jwt.extractKey(claims, "effectiveUserId")
 		if err != nil {
 			return data, err
 		}
@@ -85,21 +69,7 @@ func (jwt Decoder) Decode(tokenString string) (Payload, error) {
 	return data, errors.New("invalid claim token in jwt")
 }
 
-func fetchPublicKey(token *jwtgo.Token, jwt Decoder) *rsa.PublicKey {
-	kid, found := token.Header["kid"]
-	if !found {
-		return jwt.defaultVerifyKey
-	}
-
-	key, found := jwt.additionalKeys[kid.(string)]
-	if !found {
-		return jwt.defaultVerifyKey
-	}
-
-	return key
-}
-
-func (jwt Decoder) extractData(claims jwtgo.MapClaims, key string) (string, error) {
+func (jwt Decoder) extractKey(claims jwtgo.MapClaims, key string) (string, error) {
 	val, ok := claims[key].(string)
 	if !ok {
 		return "", fmt.Errorf("missing %s in jwt token", key)
